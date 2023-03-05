4d66ac113c7032b0e272bcf8e461aef4d6e4b896
GPN87 <gavin.payne87@gmail.com>
2023-03-06 09:03:09 +1100

    progress on charts

---
+++ static/js/app.js
@@ -0,0 +1,96 @@
+const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
+
+// READS IN DATA FROM URL
+d3.json(url).then(function(data){
+    
+    // SETS UP VARIABLES FOR REQUIRED OBJECTS
+    console.log(data);
+    let samples = data.samples;
+    let metadata = data.metadata;
+    console.log(samples);
+    console.log(metadata);
+
+    // CREATES CHARTS (FROM SAMPLE OBJECT)
+    function createChart(sampleId){
+        let sampleData = samples.filter(function (sample){
+            return sample.id == sampleId;
+        })[0];
+
+        let sortedData=sampleData.sample_values.map(function(value,index){
+            return{
+                sample_value: value,
+                otu_id: sampleData.otu_ids[index],
+                otu_labels: sampleData.otu_labels[index]
+            };
+        }).sort(function(a,b){
+            return b.sample_value - a.sample_value;
+        }).slice(0,10).reverse();
+
+        // Horizontal bar graph
+        let tracedata = {
+            x: sortedData.map(s=>s.sample_value),
+            y: sortedData.map(s=>`ID: ${s.otu_id}`),
+            type: "bar",
+            orientation: "h"
+        };
+        let layout = {
+            title: `Main OTUs detected in Sample ${sampleId}:`
+        };
+        Plotly.newPlot('bar',[tracedata],layout);
+        
+        //Bubble Graph
+        let tracedata2 = {
+            x:sortedData.map(t=>t.otu_id),
+            y:sortedData.map(t=>t.sample_value),
+            text: sortedData.map(t=>t.otu_labels),
+            mode: "markers",
+            marker: {
+                color:sortedData.map(t=>t.otu_id),
+                size:sortedData.map(t=>t.sample_value)
+            }
+        };
+        let layout2 = {
+            title: 'Bubble Chart of OTUs',
+            showlegend: false,
+            height: 500,
+            width: 1100
+        };
+        Plotly.newPlot('bubble',[tracedata2],layout2);
+    }
+
+
+    function init(){
+        let init = samples[0].id;
+        createChart(init);
+    };
+    
+    let dropdown = d3.select("#selDataset");
+    let dropdownoptions = dropdown.selectAll("option").data(samples).enter().append("option").attr("value",function(s){return s.id;}).text(function(d){return "Sample "+ d.id;});
+    dropdown.on('change',function(){
+    let selectedId = d3.select(this).property('value');
+    createChart(selectedId);
+    
+    });
+
+
+    init();
+});
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
